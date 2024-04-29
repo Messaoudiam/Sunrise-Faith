94dac2fc9e4a7feb91849b16ddbf72e5197c9d06
Messaoud ZOUGGAB <messaoud.zouggab@gmail.com>
2024-04-29 19:37:54 +0200

    function to convert date format + a function to convert hours in 24h type

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import Hours from "./components/Hours";
 function App() {
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold underline"> Sunrise </h1>
+      <div className="bg-green-100 h-screen w-screen font-roboto">
+        <h1 className="font-black text-center text-green-700 text-7xl">
+          🌄 Sunrise Faith
+        </h1>
         <Hours />
       </div>
     </>
--- src/components/Hours.jsx
+++ src/components/Hours.jsx
@@ -1,4 +1,58 @@
+// react
 import { useState } from "react";
+
+function convertTo24(AMhours) {
+  let composants = AMhours.split(":");
+  let hours = parseInt(composants[0]);
+  let minutes = parseInt(composants[1]);
+
+  let isAM = AMhours.toLowerCase().indexOf("am") !== -1;
+
+  if (hours === 12) {
+    if (isAM) {
+      hours = 0;
+    }
+  } else {
+    if (!isAM) {
+      hours += 12;
+    }
+  }
+
+  return hours + ":" + minutes.toString().padStart(2, "0");
+}
+
+function dateFormat(date) {
+  const weekDays = [
+    "Dimanche",
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+  ];
+  const months = [
+    "janvier",
+    "février",
+    "mars",
+    "avril",
+    "mai",
+    "juin",
+    "juillet",
+    "août",
+    "septembre",
+    "octobre",
+    "novembre",
+    "décembre",
+  ];
+
+  const weekDay = weekDays[date.getDay()];
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  return `${weekDay} ${day} ${month} ${year}`;
+}
 
 function Hours() {
   const [city, setCity] = useState("");
@@ -27,38 +81,45 @@ function Hours() {
   };
 
   return (
-    <div>
-      <h2>Horaires de Prière Hebdomadaires par Ville</h2>
+    <div className="flex flex-col items-center">
       <form onSubmit={handleSubmit}>
-        <label>
-          Ville:
-          <input
-            type="text"
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-          />
-        </label>
-        <button type="submit">Obtenir les Horaires de Prière</button>
+        <input
+          placeholder="Selectionnez votre ville"
+          className="w-auto border border-gray-300 rounded-md py-2 px-4 mr-2 focus:outline-none focus:border-green-500 m-5"
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button
+          className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
+          type="submit"
+        >
+          Obtenir les Horaires de Prière
+        </button>
       </form>
+
       {error && <p>Erreur: {error}</p>}
+
       {prayerTimes && (
-        <div>
-          <h3>Horaires de Prière pour la Semaine</h3>
-          <ul>
-            {prayerTimes.items.map((item, index) => (
-              <li key={index}>
-                <strong>{item.date_for}</strong>:
-                <ul>
-                  <li>Fajr: {item.fajr}</li>
-                  <li>Dhuhr: {item.dhuhr}</li>
-                  <li>Asr: {item.asr}</li>
-                  <li>Maghrib: {item.maghrib}</li>
-                  <li>Isha: {item.isha}</li>
-                </ul>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <ul className="flex gap-5 justify-center">
+          {prayerTimes.items.map((item, index) => (
+            <li
+              key={index}
+              className="flex justify-center items-center flex-col bg-white rounded-lg shadow-md p-3 mx-auto"
+            >
+              <strong className=" text-xl m-2">
+                {dateFormat(new Date(item.date_for))}
+              </strong>
+              <ul>
+                <li>Fajr: {convertTo24(item.fajr)}</li>
+                <li>Dhuhr: {convertTo24(item.dhuhr)}</li>
+                <li>Asr: {convertTo24(item.asr)}</li>
+                <li>Maghrib: {convertTo24(item.maghrib)}</li>
+                <li>Isha: {convertTo24(item.isha)}</li>
+              </ul>
+            </li>
+          ))}
+        </ul>
       )}
     </div>
   );
